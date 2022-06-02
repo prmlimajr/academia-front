7a94cd53e9acfc78debea8c51641b253badb93fb
Paulo Lima <paulo.lima@pitang.com>
2022-06-02 02:25:37 -0300

    XGH

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { FiLogOut } from 'react-icons/fi';
+import { useHistory } from 'react-router-dom';
 
 import Ladsoft from '../../assets/img/ladsoft.png';
+import { loggedOut } from '../../services/auth';
 import './Sidebar.css';
 export default function Sidebar() {
+  const history = useHistory()
+  const handleLogOut = () => {
+    loggedOut()
+    history.push('/login');
+  }
   return (
     <div className='sidebar'>
       <img src={Ladsoft} alt='Ladsoft' className='sidebarLogo' />
       <div className='row'>
-        <p className='logout'>Sair</p>
+        <p className='logout' onClick={() => handleLogOut()}>Sair</p>
         <FiLogOut size={14} color='#534f47' />
       </div>
     </div>
